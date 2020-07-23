8e5607528faa04239cab4099e2d750497f50a8db
Jennifer Tesolin <3802757+jennifert@users.noreply.github.com>
2020-07-23 12:59:51 -0400

    Fetch examples consolidated into single folder.
    In filename added git contributer and history of when sumbited.
    Basically JS folders removed (cthey were conflicting) into a folder called fetch.

--- Fetch/multiline.js
+++ Fetch/multiline.js
@@ -1,5 +1,5 @@
-
-// Fetch example
+// Fetch example by jtryan: https://github.com/jtryan
+//https://github.com/jennifert/JavaScript-Demos/commit/cd54948480699cb02913a568d003be67c86d1248#diff-96db5127afd0be38e220beae66d844dc
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 fetch(url)
     .then((resp) => resp.json()) //act on promise
--- Fetch/single_line_chain.js
+++ Fetch/single_line_chain.js
@@ -1,3 +1,6 @@
+//Example by: jcyktor: https://github.com/jcyktor
+//https://github.com/jennifert/JavaScript-Demos/commit/6a31265eb18ecc78f2db18e1f283cfac8667b3d7#diff-b29cb358d6528934f426332e9c4d5480
+
 const ticker = 'goog'; // change this to search other stocks
 const url = `https://api.iextrading.com/1.0/stock/${ticker}/price`;
 
